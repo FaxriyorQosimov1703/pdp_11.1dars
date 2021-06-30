6fa6bae3c2758a0d61cec924856fca26787a3cac
Faxriyor <faxriyor@gmail.com>
2021-06-30 17:28:47 +0300

    add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Timer from "./componet/timer";
+
+function App() {
+
+
+    return (
+        <div className={'container'}>
+            <div className="row">
+                <Timer/>
+            </div>
+        </div>
+
+    );
+}
+
+export default App; 
